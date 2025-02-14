fe0096231459bfbb823b7bf495144e65563e82ce
Hafid ZITI <ziti.hafid@gmail.com>
2025-02-14 17:27:56 +0100

    fix typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <main className={styles.main}>
-          <h1>{process.env.NEXT_PUBLIC_VERION}</h1>
+          <h1>{process.env.NEXT_PUBLIC_VERSION}</h1>
           <Image
             className={styles.logo}
             src="/next.svg"
